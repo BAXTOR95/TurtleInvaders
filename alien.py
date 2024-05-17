302f131fa3da36c8d7c606d64dc853c5ba7317e9
Brian Arriaga <brian.arriaga@gmail.com>
2024-05-16 21:05:57 -0400

    Add documentation to alien

--- alien.py
+++ alien.py
@@ -2,7 +2,28 @@ from turtle import Turtle
 
 
 class Alien(Turtle):
+    """
+    A class to represent an alien in the game.
+
+    Attributes:
+        frames (list): List of image frames for alien animation.
+        frame_index (int): Current index of the frame being displayed.
+        direction (int): Current direction of movement (1 for right, -1 for left).
+        speed (float): Speed of the alien's movement.
+
+    Methods:
+        move(): Moves the alien and changes direction at screen edges.
+        update_animation(): Updates the alien's animation frame.
+    """
+
     def __init__(self, speed, frames):
+        """
+        Initializes the alien with its animation frames and movement speed.
+
+        Args:
+            speed (float): Speed of the alien's movement.
+            frames (list): List of image frames for alien animation.
+        """
         super().__init__()
         self.frames = frames
         self.frame_index = 0
@@ -12,6 +33,9 @@ class Alien(Turtle):
         self.speed = speed
 
     def move(self):
+        """
+        Moves the alien in the current direction and changes direction at screen edges.
+        """
         new_x = self.xcor() + self.direction * self.speed
         self.goto(new_x, self.ycor())
         if self.xcor() > 350 or self.xcor() < -350:
@@ -19,5 +43,8 @@ class Alien(Turtle):
             self.goto(self.xcor(), self.ycor() - 40)
 
     def update_animation(self):
+        """
+        Updates the alien's animation frame.
+        """
         self.frame_index = (self.frame_index + 1) % len(self.frames)
         self.shape(self.frames[self.frame_index])
