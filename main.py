6683b6422b3fc89104a4ae0744924ad8b5c67ac2
Brian Arriaga <brian.arriaga@gmail.com>
2024-05-16 21:13:43 -0400

    Add some comments on main.py

--- main.py
+++ main.py
@@ -3,21 +3,29 @@ from game import Game
 
 
 def main():
+    """
+    The main function to initialize and start the Turtle Invaders game.
+    """
+    # Set up the screen
     screen = Screen()
     screen.setup(width=800, height=600)
     screen.bgcolor("black")
     screen.title("Turtle Invaders")
     screen.tracer(0)
 
+    # Create the game instance
     game = Game(screen)
 
+    # Set up key bindings
     screen.listen()
     screen.onkey(game.spaceship.move_left, "Left")
     screen.onkey(game.spaceship.move_right, "Right")
     screen.onkey(game.spaceship.shoot, "space")
 
+    # Run the game
     game.run()
 
+    # Keep the window open
     screen.mainloop()
 
 
