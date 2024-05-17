b3d96e9746d26e10aba7a77c230f0b62fa4b1df7
Brian Arriaga <brian.arriaga@gmail.com>
2024-05-16 21:05:01 -0400

    Add documentation to spaceship.py

--- spaceship.py
+++ spaceship.py
@@ -5,7 +5,33 @@ import time
 
 
 class Spaceship(Turtle):
+    """
+    A class to represent the player's spaceship.
+
+    Attributes:
+        frames (list): List of image frames for spaceship animation.
+        projectile_frames (list): List of image frames for projectile animation.
+        sound_manager (SoundManager): Manages the game's sound effects.
+        frame_index (int): Current index of the frame being displayed.
+        projectiles (list): List of active projectiles shot by the spaceship.
+        last_shot_time (float): Time when the last projectile was shot.
+
+    Methods:
+        move_left(): Moves the spaceship to the left.
+        move_right(): Moves the spaceship to the right.
+        shoot(): Shoots a projectile if the shoot delay has passed.
+        update_animation(): Updates the spaceship's animation frame.
+    """
+
     def __init__(self, frames, projectile_frames, sound_manager):
+        """
+        Initializes the spaceship with its animation frames, projectile frames, and sound manager.
+
+        Args:
+            frames (list): List of image frames for spaceship animation.
+            projectile_frames (list): List of image frames for projectile animation.
+            sound_manager (SoundManager): Manages the game's sound effects.
+        """
         super().__init__()
         self.frames = frames
         self.projectile_frames = projectile_frames
@@ -19,16 +45,25 @@ class Spaceship(Turtle):
         self.last_shot_time = 0
 
     def move_left(self):
+        """
+        Moves the spaceship to the left if it is within the screen bounds.
+        """
         new_x = self.xcor() - SHIP_SPEED
         if new_x > -SCREEN_WIDTH / 2:
             self.goto(new_x, self.ycor())
 
     def move_right(self):
+        """
+        Moves the spaceship to the right if it is within the screen bounds.
+        """
         new_x = self.xcor() + SHIP_SPEED
         if new_x < SCREEN_WIDTH / 2:
             self.goto(new_x, self.ycor())
 
     def shoot(self):
+        """
+        Shoots a projectile if the delay since the last shot has passed.
+        """
         current_time = time.time()
         if current_time - self.last_shot_time >= SHOOT_DELAY:
             projectile = Projectile(
@@ -39,5 +74,8 @@ class Spaceship(Turtle):
             self.last_shot_time = current_time
 
     def update_animation(self):
+        """
+        Updates the spaceship's animation frame.
+        """
         self.frame_index = (self.frame_index + 1) % len(self.frames)
         self.shape(self.frames[self.frame_index])
