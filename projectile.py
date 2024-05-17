f7668351e1ec1b718a09c0f179662611beff4c49
Brian Arriaga <brian.arriaga@gmail.com>
2024-05-16 21:07:12 -0400

    Add documentation for projectile.py

--- projectile.py
+++ projectile.py
@@ -3,7 +3,29 @@ from config import PROJECTILE_SPEED
 
 
 class Projectile(Turtle):
+    """
+    A class to represent a projectile in the game.
+
+    Attributes:
+        frames (list): List of image frames for projectile animation.
+        frame_index (int): Current index of the frame being displayed.
+        direction (int): Direction of the projectile's movement (1 for up, -1 for down).
+
+    Methods:
+        move(): Moves the projectile in the current direction.
+        update_animation(): Updates the projectile's animation frame.
+    """
+
     def __init__(self, x, y, direction=1, frames=[]):
+        """
+        Initializes the projectile with its initial position, direction, and animation frames.
+
+        Args:
+            x (float): Initial x-coordinate of the projectile.
+            y (float): Initial y-coordinate of the projectile.
+            direction (int): Direction of the projectile's movement (1 for up, -1 for down).
+            frames (list): List of image frames for projectile animation.
+        """
         super().__init__()
         self.frames = frames
         self.frame_index = 0
@@ -16,9 +38,15 @@ class Projectile(Turtle):
         self.direction = direction
 
     def move(self):
+        """
+        Moves the projectile forward in the current direction.
+        """
         self.forward(PROJECTILE_SPEED)
 
     def update_animation(self):
+        """
+        Updates the projectile's animation frame.
+        """
         if self.frames:
             self.frame_index = (self.frame_index + 1) % len(self.frames)
             self.shape(self.frames[self.frame_index])
